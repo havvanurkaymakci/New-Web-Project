fdf74c304c449f34ec35140e08706fd3965ea024
Havvanur Kaymakçı <hvnrkymkc@gmail.com>
2023-12-15 01:28:24 +0300

    Proje dosyası ekle.

---
+++ New Web Project/Models/FlightSystemContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using New_Web_Project.Models.Login;
+using New_Web_Project.Controllers;
+
+
+namespace New_Web_Project.Models
+{
+    public class FlightSystemContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=HAVVANUR\MSSQLSERVER01; 
+             Database=FlightSystem;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ New Web Project/Models/Login/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace New_Web_Project.Models.Login
+{
+    public class User
+    {
+
+        [Key]
+        public int UserId { get; set; }
+
+
+        [EmailAddress(ErrorMessage = "Lütfen geçerli bir mail giriniz")]
+        [Required(ErrorMessage = "Mail adresi alanı zorunludur.")]
+        public string UserMail { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Lütfen şifre belirleyiniz")]
+        [MaxLength(15, ErrorMessage = "Şifre 15 karakterdan uzun olamaz."),
+        MinLength(8, ErrorMessage = "Şifre 8 karakterden kısa olamaz.")]
+        public string UserPassword { get; set; }
+    }
+}
